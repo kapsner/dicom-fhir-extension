b07ff82e759458debac17b598d6ae734cb18a74a
kapsner <lorenz.kapsner@gmail.com>
2023-04-23 09:03:21 +0200

    first commit

---
+++ ImagingStudySeries.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+__author__ = "Lorenz A. Kapsner"
+
+from fhir import resources as fr
+import typing
+from pydantic import Field
+
+
+class ImagingStudySeriesErlangen(fr.imagingstudy.ImagingStudySeries):
+    # general image
+
+    # MR-specific
+    scanningSequence__ext: fr.fhirtypes.CodingType = Field(
+        None,
+        alias="scanningSequence",
+        title="All of the distinct values for series' scanning sequence",
+        description=("Description of the type of data taken."),
+        # if property is element of this resource.
+        element_property=True,
+    )
+    scanningVariant__ext: fr.fhirtypes.CodeableConceptType = Field(
+        None,
+        alias="scanningVariant",
+        title="All of the distinct values for series' scanning variant",
+        description=("Variant of the Scanning Sequence."),
+        # if property is element of this resource.
+        element_property=True,
+    )
+    echoTimes__ext: fr.fhirtypes.Decimal = Field(
+        None,
+        alias="echoTimes",
+        title="Echo Time",
+        description="Echo Times",
+        # if property is element of this resource.
+        element_property=True,
+    )
+
+    @classmethod
+    def elements_sequence(cls):
+        """returning all elements names from
+        ``ImagingStudySeries`` according specification,
+        with preserving original sequence order.
+        """
+        return [
+            "id",
+            "extension",
+            "modifierExtension",
+            "uid",
+            "number",
+            "modality",
+            "description",
+            "numberOfInstances",
+            "endpoint",
+            "bodySite",
+            "laterality",
+            "specimen",
+            "started",
+            "performer",
+            "instance",
+            "scanningSequence",
+            "scanningVariant",
+            "echoTimes",
+        ]
